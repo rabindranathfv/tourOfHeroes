83027c7b4126517360930b1846fd7d0e37ac0e47
Rabindranath <ferreiralox@gmail.com>
2018-11-02 19:12:51 -0300

    add heroesService, add Observable,of method from  Rxjs, get asyncronnys data in a heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../data/mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +8,24 @@ import { HEROES } from '../data/mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  // get data fron a heroService
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => {
+          this.heroes = heroes;
+        });
   }
 }
