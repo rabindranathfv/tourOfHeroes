a9258993029741053da2d19470c4d8187b3c3bc7
Rabindranath <ferreiralox@gmail.com>
2018-11-02 19:23:30 -0300

    moving all service into a service folder a editing all imports...works fine

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
-import { Hero } from './hero';
-import { HEROES } from './data/mock-heroes';
+import { Hero } from '../hero';
+import { HEROES } from '../data/mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     return of(HEROES);
